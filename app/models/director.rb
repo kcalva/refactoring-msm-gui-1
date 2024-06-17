44b5cdde7eeac228088c0c110d279d87a3a985a7
Kevin <kevin.calva@us.isobar.com>
2024-06-16 22:49:30 -0500

    refactoring (still need to go over this)

--- app/models/director.rb
+++ app/models/director.rb
@@ -11,4 +11,23 @@
 #  updated_at :datetime         not null
 #
 class Director < ApplicationRecord
+  def filmography
+    my_id = self.id
+    matching_movies = Movie.where({:director_id=>my_id})
+    return matching_movies
+  end
+
+
+  #Three 1-N associations
+  #-Director =>Movie
+  #-Movie => Character
+  #-Actor => Character
+
+
+  #try define:
+  #-Director#filmography
+  #-Movie#characters
+  #-Character#movie
+  #-Actor#character
+  #-Character#actor
 end
